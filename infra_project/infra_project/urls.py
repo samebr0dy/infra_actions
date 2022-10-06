18d9b0ddecb74f32a8cd5f047ab225837849c847
Илья Шустров <shustrov.i@ya.ru>
2022-10-06 23:28:34 +0300

    reload

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
